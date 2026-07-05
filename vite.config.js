c3946b51c9786ec2c6ecee89db4486c7dc64ba24
Alwin Jebaraj <alwinjebaraj76@gmail.com>
2026-07-05 13:29:51 +0530

    Fix string quotes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/netclone/',
+    base: "/netclone/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
